4856fe3e05bf52fee313b0999baeab3ea0423c09
UpcraftLP <j-schirra@gmx.de>
2019-03-30 21:09:38 +0100

    initial release

---
+++ src/main/java/nerdhub/cardinal/components/api/ComponentType.java
@@ -0,0 +1,19 @@
+package nerdhub.cardinal.components.api;
+
+public class ComponentType<T> {
+
+    /**
+     * package-private;
+     * @see ComponentRegistry#get(Class)!
+     */
+    ComponentType() {
+    }
+
+    /**
+     * convenience method to easily cast a component instance to it's type
+     */
+    @SuppressWarnings("unchecked")
+    public <V> V cast(Object instance) {
+        return (V) instance;
+    }
+}
