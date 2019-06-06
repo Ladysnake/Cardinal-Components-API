1f29a13b97a8254132a9a2fc56cd4d3251af0f20
UpcraftLP <j-schirra@gmx.de>
2019-06-06 15:36:51 +0200

    add optional getters to component providers

--- src/main/java/nerdhub/cardinal/components/api/accessor/EntityComponentAccessor.java
+++ src/main/java/nerdhub/cardinal/components/api/accessor/EntityComponentAccessor.java
@@ -5,6 +5,7 @@ import nerdhub.cardinal.components.api.component.Component;
 import net.minecraft.entity.Entity;
 
 import javax.annotation.Nullable;
+import java.util.Optional;
 import java.util.Set;
 
 public interface EntityComponentAccessor {
@@ -17,6 +18,10 @@ public interface EntityComponentAccessor {
 
     @Nullable
     <T extends Component> T getComponent(ComponentType<T> type);
+
+    default <T extends Component> Optional<T> optionally(ComponentType<T> type) {
+        return Optional.ofNullable(getComponent(type));
+    }
 
     Set<ComponentType<? extends Component>> getComponentTypes();
 
