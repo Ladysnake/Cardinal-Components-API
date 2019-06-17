4d692d3eac34aa867a1df47dc1492bf7340433bb
Pyrofab <redstoneinfire@gmail.com>
2019-06-18 00:27:03 +0200

    Add test skeleton for IndexedComponentContainer

---
+++ src/test/java/nerdhub/cardinal/components/impl/IndexedComponentContainerTest.java
@@ -0,0 +1,46 @@
+package nerdhub.cardinal.components.impl;
+
+import org.junit.jupiter.api.Test;
+
+class IndexedComponentContainerTest {
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void containsKey() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void get1() {
+    }
+
+    @Test
+    void put() {
+    }
+
+    @Test
+    void fromTag() {
+    }
+
+    @Test
+    void toTag() {
+    }
+
+    @Test
+    void keySet() {
+    }
+
+    @Test
+    void values() {
+    }
+
+    @Test
+    void entrySet() {
+    }
+}
