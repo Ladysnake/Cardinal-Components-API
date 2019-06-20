e95cb809ddf3521caf81e1196e3e38169f1758bf
Pyrofab <redstoneinfire@gmail.com>
2019-06-21 00:25:35 +0200

    Make the BE components event type based rather than class based

--- src/main/java/nerdhub/cardinal/components/internal/BlockEntityTypeCaller.java
+++ src/main/java/nerdhub/cardinal/components/internal/BlockEntityTypeCaller.java
@@ -1,7 +1,9 @@
 package nerdhub.cardinal.components.internal;
 
 import nerdhub.cardinal.components.api.event.BlockEntityComponentCallback;
+import net.fabricmc.fabric.api.event.Event;
+import net.minecraft.block.entity.BlockEntity;
 
-public interface BlockEntityTypeCaller {
-	BlockEntityComponentCallback getBlockEntityComponentEvent();
+public interface BlockEntityTypeCaller<B extends BlockEntity> {
+	Event<BlockEntityComponentCallback<B>> getBlockEntityComponentEvent();
 }
--- src/main/java/nerdhub/cardinal/components/mixins/common/MixinBlockEntityType.java
+++ src/main/java/nerdhub/cardinal/components/mixins/common/MixinBlockEntityType.java
@@ -3,40 +3,35 @@ package nerdhub.cardinal.components.mixins.common;
 import nerdhub.cardinal.components.api.component.SidedContainerCompound;
 import nerdhub.cardinal.components.api.event.BlockEntityComponentCallback;
 import nerdhub.cardinal.components.api.event.EntityComponentCallback;
+import nerdhub.cardinal.components.internal.BlockEntityTypeCaller;
 import net.fabricmc.fabric.api.event.Event;
+import net.fabricmc.fabric.api.event.EventFactory;
 import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import org.spongepowered.asm.mixin.Mixin;
 
-import javax.annotation.Nullable;
-import java.util.ArrayList;
-import java.util.List;
-
 @Mixin(EntityType.class)
-public abstract class MixinBlockEntityType<B extends BlockEntity> {
-    @Nullable
-	private Event<BlockEntityComponentCallback<? super B>>[] cardinal_componentEvents;
+public abstract class MixinBlockEntityType<B extends BlockEntity> implements BlockEntityTypeCaller<B> {
+    @SuppressWarnings("unchecked")
+    private final Event<BlockEntityComponentCallback<B>> cardinal_componentEvent =
+            (Event<BlockEntityComponentCallback<B>>) (Event) EventFactory.createArrayBacked(BlockEntityComponentCallback.class, callbacks -> (be, components) -> {
+                for (BlockEntityComponentCallback callback : callbacks) {
+                    callback.attachComponents(be, components);
+                }
+            });
+
+    @Override
+    public Event<BlockEntityComponentCallback<B>> getBlockEntityComponentEvent() {
+        return cardinal_componentEvent;
+    }
 
     /**
      * Fires {@link EntityComponentCallback} for every superclass of the given entity.
      * <strong>{@code b} MUST have this entity type.</strong>
      * This method has undefined behaviour if several entity classes share the same entity type.
      */
-    @SuppressWarnings("unchecked")
     void cardinal_fireComponentEvents(B b, SidedContainerCompound cc) {
         // assert b.getType() == this;
-        if (cardinal_componentEvents == null) {
-            List<Event<BlockEntityComponentCallback<? super B>>> events = new ArrayList<>();
-            Class c = b.getClass();
-            while (Entity.class.isAssignableFrom(c)) {
-                events.add(BlockEntityComponentCallback.event(c));
-                c = c.getSuperclass();
-            }
-            cardinal_componentEvents = events.toArray(new Event[0]);
-        }
-        for (Event<BlockEntityComponentCallback<? super B>> event : cardinal_componentEvents) {
-            event.invoker().attachComponents(b, cc);
-        }
+        cardinal_componentEvent.invoker().attachComponents(b, cc);
     }
 }
