7dcf154869bdabafee889253fa6056ff9cf03789
Pyrofab <redstoneinfire@gmail.com>
2019-06-21 00:23:30 +0200

    Implement the Item components event

--- src/main/java/nerdhub/cardinal/components/api/event/ItemComponentCallback.java
+++ src/main/java/nerdhub/cardinal/components/api/event/ItemComponentCallback.java
@@ -1,4 +1,17 @@
 package nerdhub.cardinal.components.api.event;
 
-public interface ItemComponentCallback {
+import nerdhub.cardinal.components.api.component.ComponentContainer;
+import nerdhub.cardinal.components.internal.ItemCaller;
+import net.fabricmc.fabric.api.event.Event;
+import net.minecraft.item.Item;
+
+@FunctionalInterface
+public interface ItemComponentCallback<T extends Item> {
+    @SuppressWarnings("unchecked")
+    static <T extends Item> Event<ItemComponentCallback<T>> event(T item) {
+        return (Event<ItemComponentCallback<T>>) (Event) ((ItemCaller)item).getItemComponentEvent();
+    }
+
+    void attachComponents(T item, ComponentContainer components);
+
 }
