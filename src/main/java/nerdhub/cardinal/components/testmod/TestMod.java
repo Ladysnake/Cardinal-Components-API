f7ce80ab5f1f1ddc521c8c465a4c6c56092634b1
Pyrofab <redstoneinfire@gmail.com>
2019-06-22 15:51:46 +0200

    Move the test mod to a separate source set

--- src/main/java/nerdhub/cardinal/components/testmod/TestMod.java
+++
@@ -1,7 +0,0 @@
-package nerdhub.cardinal.components.testmod;
-
-public final class TestMod {
-    public static void onInitialize() {
-
-    }
-}
---
+++ src/testmod/java/nerdhub/cardinal/componentstest/CardinalComponentsTest.java
@@ -0,0 +1,7 @@
+package nerdhub.cardinal.componentstest;
+
+public class CardinalComponentsTest {
+    public static void init() {
+        System.out.println("Hello, Components!");
+    }
+}
