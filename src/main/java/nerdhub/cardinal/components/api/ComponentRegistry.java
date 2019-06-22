c0190d17c1db1be51254f48b230021ef13cecd89
Pyrofab <redstoneinfire@gmail.com>
2019-06-22 19:43:19 +0200

    Write unit tests for component registry

--- src/main/java/nerdhub/cardinal/components/api/ComponentRegistry.java
+++ src/main/java/nerdhub/cardinal/components/api/ComponentRegistry.java
@@ -4,6 +4,7 @@ import nerdhub.cardinal.components.api.component.Component;
 import nerdhub.cardinal.components.internal.ComponentRegistryImpl;
 import net.minecraft.util.Identifier;
 
+import javax.annotation.Nullable;
 import java.util.stream.Stream;
 
 public interface ComponentRegistry {
@@ -13,12 +14,16 @@ public interface ComponentRegistry {
      * used to obtain a shared instance of {@link ComponentType}
      *
      * @param componentClass the interface of which to obtain a {@link ComponentType}
+     * @throws IllegalArgumentException if {@code componentClass} is not an interface
+     * @throws IllegalArgumentException if {@code componentClass} does not extend {@link Component}
+     * @throws IllegalStateException if a different component class has been registered with the same {@code componentId}
      */
     <T extends Component> ComponentType<T> registerIfAbsent(Identifier componentId, Class<T> componentClass);
 
     /**
      * Directly retrieves a ComponentType from its id.
      */
+    @Nullable
     ComponentType<?> get(Identifier id);
 
     Stream<ComponentType<?>> stream();
