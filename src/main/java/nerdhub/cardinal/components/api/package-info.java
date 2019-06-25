455d216e43fe8f77cc7b57295afdb5a999279bdd
Pyrofab <redstoneinfire@gmail.com>
2019-06-25 16:41:48 +0200

    Document component registry

---
+++ src/main/java/nerdhub/cardinal/components/api/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Root package for Cardinal-Component's API
+ */
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+package nerdhub.cardinal.components.api;
+
+import nerdhub.cardinal.components.api.util.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
