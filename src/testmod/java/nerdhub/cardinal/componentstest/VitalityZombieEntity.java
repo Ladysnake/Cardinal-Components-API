5c721cafa6792f15ff7a42db1bf848ed1d958b99
Pyrofab <redstoneinfire@gmail.com>
2019-06-29 22:46:43 +0200

    Document the test mod a bit

--- src/testmod/java/nerdhub/cardinal/componentstest/VitalityZombieEntity.java
+++ src/testmod/java/nerdhub/cardinal/componentstest/VitalityZombieEntity.java
@@ -3,11 +3,20 @@ package nerdhub.cardinal.componentstest;
 import nerdhub.cardinal.components.api.component.ComponentContainer;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.mob.ZombieEntity;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.world.World;
 
 public class VitalityZombieEntity extends ZombieEntity {
     public VitalityZombieEntity(EntityType<? extends ZombieEntity> type, World world) {
         super(type, world);
+    }
+
+    @Override
+    public void tickMovement() {
+        super.tickMovement();
+        if (this.world.isClient) {
+            this.world.addParticle(ParticleTypes.DRAGON_BREATH, this.x, this.y + 0.3D, this.z, this.random.nextGaussian() * 0.05D, this.random.nextGaussian() * 0.05D, this.random.nextGaussian() * 0.05D);
+        }
     }
 
     protected void initComponents(ComponentContainer components) {
