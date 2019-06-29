7a7610971fd0de5c2730f12cf94b718fd7ed9dee
Pyrofab <redstoneinfire@gmail.com>
2019-06-29 23:50:04 +0200

    Add basic check in block provider implementation

--- src/main/java/nerdhub/cardinal/components/mixins/common/MixinBlock.java
+++ src/main/java/nerdhub/cardinal/components/mixins/common/MixinBlock.java
@@ -32,15 +32,20 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.BlockView;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 
 import javax.annotation.Nullable;
+import java.util.Collections;
 import java.util.Set;
 
 @Mixin(Block.class)
-public class MixinBlock implements BlockComponentProvider {
+public abstract class MixinBlock implements BlockComponentProvider {
+
+    @Shadow public abstract boolean hasBlockEntity();
 
     @Override
     public <T extends Component> boolean hasComponent(BlockView blockView, BlockPos pos, ComponentType<T> type, @Nullable Direction side) {
+        if (!this.hasBlockEntity()) return false;
         BlockEntity be = blockView.getBlockEntity(pos);
         return be instanceof BlockComponentProvider && ((BlockComponentProvider) be).hasComponent(blockView, pos, type, side);
     }
@@ -48,12 +53,14 @@ public class MixinBlock implements BlockComponentProvider {
     @Nullable
     @Override
     public <T extends Component> T getComponent(BlockView blockView, BlockPos pos, ComponentType<T> type, @Nullable Direction side) {
+        if (!this.hasBlockEntity()) return null;
         BlockEntity be = blockView.getBlockEntity(pos);
         return be instanceof BlockComponentProvider ? ((BlockComponentProvider) be).getComponent(blockView, pos, type, side) : null;
     }
 
     @Override
     public Set<ComponentType<? extends Component>> getComponentTypes(BlockView blockView, BlockPos pos, @Nullable Direction side) {
+        if (!this.hasBlockEntity()) return Collections.emptySet();
         BlockEntity be = blockView.getBlockEntity(pos);
         return be instanceof BlockComponentProvider ? ((BlockComponentProvider) be).getComponentTypes(blockView, pos, side) : ImmutableSet.of();
     }
