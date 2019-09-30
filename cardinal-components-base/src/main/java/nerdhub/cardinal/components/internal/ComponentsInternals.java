7d3db6f3a3c9993e69685d1a57bee1f016388e36
Pyrofab <redstoneinfire@gmail.com>
2019-09-30 11:32:45 +0200

    Remove an indirection in the terrible mess

--- cardinal-components-base/src/main/java/nerdhub/cardinal/components/internal/ComponentsInternals.java
+++ cardinal-components-base/src/main/java/nerdhub/cardinal/components/internal/ComponentsInternals.java
@@ -1,5 +1,6 @@
 package nerdhub.cardinal.components.internal;
 
+import nerdhub.cardinal.components.api.ComponentType;
 import nerdhub.cardinal.components.api.component.Component;
 import nerdhub.cardinal.components.api.component.ComponentContainer;
 import nerdhub.cardinal.components.api.event.ComponentCallback;
@@ -11,6 +12,7 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 public final class ComponentsInternals {
     private static final Field EVENT$TYPE;
@@ -22,7 +24,7 @@ public final class ComponentsInternals {
         try {
             EVENT$TYPE = Class.forName("net.fabricmc.fabric.impl.event.ArrayBackedEvent").getDeclaredField("type");
             EVENT$TYPE.setAccessible(true);
-            IMPL_HANDLE = LOOKUP.findStatic(ComponentsInternals.class, "initComponentsWrapper", MethodType.methodType(void.class, ComponentCallback.class, Object.class, ComponentContainer.class));
+            IMPL_HANDLE = LOOKUP.findStatic(ComponentsInternals.class, "initComponents", MethodType.methodType(void.class, ComponentType.class, Function.class, Object.class, ComponentContainer.class));
         } catch (NoSuchFieldException | ClassNotFoundException e) {
             throw new RuntimeException("Failed to hack fabric API", e);
         } catch (NoSuchMethodException | IllegalAccessException e) {
@@ -31,23 +33,27 @@ public final class ComponentsInternals {
     }
 
     @SuppressWarnings("unchecked")
-    public static <X, Y extends Component, E extends ComponentCallback<X, ? super Y>> E createCallback(Event<E> event, ComponentCallback<X, Y> impl) {
+    public static <E extends ComponentCallback<P, ? super C>, T extends Component, P, C extends T> E createCallback(Event<E> event, ComponentType<T> type, Function<P,C> factory) {
         try {
-            return (E) FACTORY_CACHE.computeIfAbsent(event, ComponentsInternals::createCallbackFactory).invoke(impl);
+            return (E) FACTORY_CACHE.computeIfAbsent(event, ComponentsInternals::createCallbackFactory).invoke(type, factory);
         } catch (Throwable t) {
             throw new RuntimeException("Failed to instantiate hacky component callback implementation", t);
         }
     }
 
+    private static <E extends ComponentCallback<P, ? super C>, T extends Component, P, C extends T> void initComponents(ComponentType<T> type, Function<P, C> factory, P provider, ComponentContainer<C> components) {
+        components.put(type, factory.apply(provider));
+    }
+
     @SuppressWarnings("unchecked")
-    public static MethodHandle createCallbackFactory(Event<?> event) {
+    private static MethodHandle createCallbackFactory(Event<?> event) {
         try {
             Class<? extends ComponentCallback> eventType = (Class<? extends ComponentCallback>) EVENT$TYPE.get(event);
             MethodType eventSamType = findSam(eventType);
             return LambdaMetafactory.metafactory(
                 LOOKUP,
                 "initComponents",
-                MethodType.methodType(eventType, ComponentCallback.class),
+                MethodType.methodType(eventType, ComponentType.class, Function.class),
                 eventSamType,
                 IMPL_HANDLE,
                 eventSamType
@@ -59,15 +65,11 @@ public final class ComponentsInternals {
         }
     }
 
-    private static<X, Y extends Component> void initComponentsWrapper(ComponentCallback<X, Y> wrapped, X provider, ComponentContainer<Y> components) {
-        wrapped.initComponents(provider, components);
-    }
-
     private static MethodType findSam(Class<? extends ComponentCallback> callbackClass) {
         try {
             for (Method m : callbackClass.getMethods()) {
                 if (Modifier.isAbstract(m.getModifiers())) {
-                    return LOOKUP.unreflect(m).type().dropParameterTypes(0, 1);
+                    return LOOKUP.unreflect(m).type().dropParameterTypes(0, 1); // drop <this>
                 }
             }
         } catch (IllegalAccessException e) {
