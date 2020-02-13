5722023e0b9d73b0c344d9d867a414c2bf0d2e0a
Pyrofab <redstoneinfire@gmail.com>
2020-02-13 17:28:55 +0100

    Update to 20w06a

--- cardinal-components-chunk/src/main/java/nerdhub/cardinal/components/api/util/sync/ChunkSyncedComponent.java
+++ cardinal-components-chunk/src/main/java/nerdhub/cardinal/components/api/util/sync/ChunkSyncedComponent.java
@@ -29,7 +29,7 @@ import nerdhub.cardinal.components.api.util.ChunkComponent;
 import net.fabricmc.fabric.api.network.PacketContext;
 import net.fabricmc.fabric.api.network.ServerSidePacketRegistry;
 import net.fabricmc.fabric.api.server.PlayerStream;
-import net.minecraft.client.network.packet.CustomPayloadS2CPacket;
+import net.minecraft.network.packet.s2c.play.CustomPayloadS2CPacket;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.PacketByteBuf;
--- cardinal-components-world/src/main/java/nerdhub/cardinal/components/mixins/common/world/MixinServerWorld.java
+++ cardinal-components-world/src/main/java/nerdhub/cardinal/components/mixins/common/world/MixinServerWorld.java
@@ -26,7 +26,6 @@ import nerdhub.cardinal.components.internal.world.ComponentPersistentState;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.WorldGenerationProgressListener;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.profiler.Profiler;
 import net.minecraft.world.PersistentStateManager;
 import net.minecraft.world.WorldSaveHandler;
 import net.minecraft.world.dimension.DimensionType;
@@ -48,7 +47,7 @@ public abstract class MixinServerWorld extends MixinWorld {
     private static final String PERSISTENT_STATE_KEY = "cardinal_world_components";
 
     @Inject(at = @At("RETURN"), method = "<init>")
-    private void constructor(MinecraftServer server, Executor executor, WorldSaveHandler oldWorldSaveHandler, LevelProperties levelProperties, DimensionType dimensionType, Profiler profiler, WorldGenerationProgressListener worldGenerationProgressListener, CallbackInfo ci) {
+    private void constructor(MinecraftServer server, Executor workerExecutor, WorldSaveHandler worldSaveHandler, LevelProperties properties, DimensionType dimensionType, WorldGenerationProgressListener worldGenerationProgressListener, CallbackInfo ci) {
         PersistentStateManager persistentStateManager = this.getPersistentStateManager();
         persistentStateManager.getOrCreate(() -> new ComponentPersistentState(PERSISTENT_STATE_KEY, this.components), PERSISTENT_STATE_KEY);
     }
