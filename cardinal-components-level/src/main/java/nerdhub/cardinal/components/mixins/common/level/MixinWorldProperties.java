f9fedfc53febe99bf9591b2442f9ee8b7e1d3a5c
Pyrofab <redstoneinfire@gmail.com>
2020-05-31 10:09:45 +0200

    Remove default methods from WorldProperties

--- cardinal-components-level/src/main/java/nerdhub/cardinal/components/mixins/common/level/MixinWorldProperties.java
+++ cardinal-components-level/src/main/java/nerdhub/cardinal/components/mixins/common/level/MixinWorldProperties.java
@@ -22,30 +22,11 @@
  */
 package nerdhub.cardinal.components.mixins.common.level;
 
-import nerdhub.cardinal.components.api.ComponentType;
-import nerdhub.cardinal.components.api.component.Component;
 import nerdhub.cardinal.components.api.component.ComponentProvider;
 import net.minecraft.world.WorldProperties;
 import org.spongepowered.asm.mixin.Mixin;
 
-import javax.annotation.Nullable;
-import java.util.Set;
-
 @Mixin(WorldProperties.class)
 public interface MixinWorldProperties extends ComponentProvider {
-    @Override
-    default boolean hasComponent(ComponentType<?> type) {
-        throw new UnsupportedOperationException(this.getClass() + " does not implement " + ComponentProvider.class + " from Cardinal-Components-API");
-    }
 
-    @Nullable
-    @Override
-    default <C extends Component> C getComponent(ComponentType<C> type) {
-        throw new UnsupportedOperationException(this.getClass() + " does not implement " + ComponentProvider.class + " from Cardinal-Components-API");
-    }
-
-    @Override
-    default Set<ComponentType<?>> getComponentTypes() {
-        throw new UnsupportedOperationException(this.getClass() + " does not implement " + ComponentProvider.class + " from Cardinal-Components-API");
-    }
 }
