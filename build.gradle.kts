3f176c8f19aaec329ba6fca63e0ef78970600a2f
Up <10714589+UpcraftLP@users.noreply.github.com>
2025-10-03 19:09:43 +0200

    revert loom workaround

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import net.fabricmc.loom.task.RemapJarTask
 import java.net.URI
 
 plugins {
-    id("fabric-loom") version "1.11-SNAPSHOT"
+    id("fabric-loom") version "1.12.0-alpha.25"
     id("io.github.ladysnake.chenille") version "0.15.0"
     id("org.cadixdev.licenser") version "0.6.1"
 }
